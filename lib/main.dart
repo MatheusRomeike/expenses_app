00b48e58c48b3f87498bf6ede2d9c081907ac286
Matheus Romeike <matheuskauva2017@hotmail.com>
2022-09-13 11:03:26 -0300

    added cards in body

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Despesas Pessoais"),
       ),
-      body: const Center(
-        child: Text("Versão Inicial"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // ignore: sized_box_for_whitespace
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text("Gráfico"),
+            ),
+          ),
+          const Card(
+            child: Text("Lista de Transações"),
+          ),
+        ],
       ),
     );
   }
